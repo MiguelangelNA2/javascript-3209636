dc6bce003e8caccdbf304ee4672f507a08d67643
miguel angel arango <mnorenaarango@gmail.com>
2025-10-16 15:29:07 -0500

    Add files via upload

--- 1-escenario-interactivo/app.js
+++ 1-escenario-interactivo/app.js
@@ -1,17 +1,1 @@
-let boton = document.querySelector(".boton");
-let body = document.querySelector("body");
-
-boton.addEventListener("click", cambiarColorFondo);
-
-function cambiarColorFondo() {
-    if (body.style.backgroundColor === "black") {
-        body.style.backgroundColor = "white";
-        body.style.color = "black";
-    } else {
-        body.style.backgroundColor = "black";
-        body.style.color = "white";
-        boton.style.backgroundColor = "Black";
-        boton.style.color = "White";
-    }
-}
 
