3a744ae5d0b9e22d277c34353c7234aef0bb8226
miguel angel arango <mnorenaarango@gmail.com>
2025-10-20 15:30:01 -0500

    Add files via upload
    animaciones e intentos de hacerlas cliqueables

--- javascript-3209636/1-escenario-interactivo/app.js
+++ javascript-3209636/1-escenario-interactivo/app.js
@@ -2,24 +2,34 @@
 let contadorMonedas = document.querySelector('.contadormonedas');
 let contadorPatos = document.querySelector('.contadorpatos');
 
-// Elementos de puntuación
-let coinsDisplay = document.querySelector('.coins');
-let scoreDisplay = document.querySelector('.score');
-
 // Monedas individuales
-let moneda1 = document.querySelector('.moneda');
+let moneda1 = document.querySelector('.m1');
 let moneda2 = document.querySelector('.m2');
 let moneda3 = document.querySelector('.m3');
 let moneda4 = document.querySelector('.m4');
 let moneda5 = document.querySelector('.m5');
 
 // todas las monedas
-let monedas = [moneda1, moneda2, moneda3, moneda4, moneda5];
+const monedas = document.querySelectorAll('.moneda');
 
 // Patos
 let pato1 = document.querySelector('.pato');
 let pato2 = document.querySelector('.p2');
 
+//nube
+let nube = document.querySelector('.nube');
+
 // Variables de puntos
 let puntosMonedas = 0;
 let puntosPatos = 0;
+
+monedas.addEventListener('click', function(e) {
+    let monedaClickeada = e.target;
+    monedaClickeada.classList.add('saltar');
+    puntosMonedas++;
+    contadorMonedas.textContent = puntosMonedas;
+});
+
+nube.addEventListener('click', function() {
+    nube.classList.add('movernube');
+});
