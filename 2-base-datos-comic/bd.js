cdfbcad6348401439d7d05d17bec414112f9cdf0
miguel angel arango <mnorenaarango@gmail.com>
2025-11-06 15:19:55 -0500

    Add files via upload

---
+++ 2-base-datos-comic/bd.js
@@ -0,0 +1,39 @@
+let comic = {
+  "NombreComic": "Cenizas de honor",
+  "Sinopsis": "un caballero a traves de la epoca medieval",
+  "Year": 2016,
+  "autores": ["miguel","alexis","luna","isabel"],
+  "Personajes": ["sir leon","lady elvira","ninnove","brujas del alba","rey","sirvienta","cristobal colon"],
+  "genero": ["historia","fantasia","ciencia ficcion","aventura"],
+  "portada": "./imagenes/portada.jpg",
+  "escenas": [
+    {
+      "id": 1,
+      "nombre": 29,
+      "personajes": ["sir leon","lady elvira"],
+      "image": "./imagenes/portada1.jpg"
+    },
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+}
