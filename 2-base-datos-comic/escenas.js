4ebb5154bb7fd1fb0c6ef2a9faded602272599cb
miguel angel arango <mnorenaarango@gmail.com>
2025-12-10 14:52:09 -0500

    Add files via upload
    botones anterior y siguiente funcionales

--- 2-base-datos-comic/escenas.js
+++ 2-base-datos-comic/escenas.js
@@ -5,14 +5,23 @@ const param = new URLSearchParams(window.location.search)
 const id = param.get("id")
 const escenas = comic.escenas.find(p => p.id == id)
 const contenedor = document.querySelector (".contenedorEscena")
+let indiceActual = comic.escenas.findIndex(p => p.id == id);
+let indiceAnterior = indiceActual - 1;
+let indiceSiguiente = indiceActual + 1;
+if (indiceAnterior < 0){
+    indiceAnterior = comic.escenas.length -1;
+}
+if (indiceSiguiente >= comic.escenas.length){
+    indiceSiguiente = 0;
+}
 contenedor.innerHTML = `
       <div class="videoescena">
-                <video src="${escenas.video}" controls width="1000"></video>
+                <video src="${escenas.video}" controls width="100%"></video>
         </div>
         <div class="botones">
-            <button class="anterior">anterior</button>
-            <button class="escenas">escenas</button>
-            <button class="siguiente">siguiente</button>
+            <a href="escenas.html?id=${comic.escenas[indiceAnterior].id}"><button class="anterior">anterior</button></a>
+            <a href="index.html"><button class="escenas">inicio</button></a>
+            <a href="escenas.html?id=${comic.escenas[indiceSiguiente].id}"><button class="siguiente">siguiente</button></a>
 
         </div>
         <div class="descripcionEscena">
